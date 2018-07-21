027cb052d21e57fc6370f24d375f3dace8ab8549
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-07-21 17:10:25 +0900

    Initial Commit

---
+++ clova-takizawa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clova-takizawa
+//
+//  Created by 下村一将 on 2018/07/21.
+//  Copyright © 2018 kazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
